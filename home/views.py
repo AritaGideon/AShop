ea63cf2bbbbed079e82bf782245cf2c220d4fda6
AritaGideon <orinagideon@gmail.com>
2024-09-26 09:52:06 +0300

    addedd function to handle errors

--- home/views.py
+++ home/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from products.forms import ProductSearchForm
 
 
 def home(request):
-    return render(request, 'home/home.html')
+    form = ProductSearchForm()
+    return render(request, 'home/home.html', {'form': form})
+
 
--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class ProductSearchForm(forms.Form):
-    query = forms.CharField(label='Search', max_length=50)
+    query = forms.CharField(max_length=30, required=False, widget=forms.TextInput
+    (attrs={'placeholder': 'Search', 'Class': 'form-control me-2'}))
 
 
 
